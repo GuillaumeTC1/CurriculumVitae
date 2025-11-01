18e22d72622fa41a0f43bf8f5d6c48643a5ccfa3
Guillaume TC <90749968+GuillaumeTC1@users.noreply.github.com>
2025-11-01 18:29:17 +0100

    Implement mail service (#10)
    * Init mail service implementation.
    
    * Refactorisation du service de messagerie
    
    Remplacement du contrôleur `MailingController` par `MailController` et du service `IMailingService` par `IMailService` dans un nouvel espace de noms. Ajout de la dépendance `AntiXSS` pour améliorer la sécurité. Mise à jour de `Program.cs` pour utiliser `AddMailServices`. Modification des options de configuration dans `appsettings.json` et changement du port SMTP pour des connexions sécurisées. Amélioration de `ContactPage.tsx` avec `axios` et des notifications pour l'envoi de messages. Ajout de `DependencyInjection.cs` pour l'injection de dépendances, et de `MailOptions.cs` pour les options de configuration. `MailService` utilise `SmtpClient` et sécurise le contenu HTML des emails.
    
    * Clean up
    
    ---------
    
    Co-authored-by: GuillaumeTC1 <gcastelnau@laposte.net>

--- curriculumvitae.client/src/pages/ContactPage.tsx
+++ curriculumvitae.client/src/pages/ContactPage.tsx
@@ -1,11 +1,39 @@
 import { useAuthContext } from "@/components/auth/context";
 import { Page } from "@/components/page/Page";
-import { Button, Form, Input, Space } from "antd";
+import { Button, Form, Input, Space, notification } from "antd";
+import axios from "axios";
+import { useState } from "react";
 
 const ContactPage = () => {
 
     const authContext = useAuthContext();
+    const [notificationApi, notificationHolder] = notification.useNotification();
     const [form] = Form.useForm();
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleSubmit = () => {
+        // Get form prompt and clear input 
+        const content = form.getFieldValue("content");
+        form.resetFields();
+
+        setLoading(true); // Set loading to display loading animation while API is responding
+
+        // Send the prompt to the chat API
+        axios.post<string>("/mail", { content })
+            .then(_ => {
+                notificationApi.success({
+                    message: "Message Sent",
+                    description: "Your message has been sent successfully."
+                });
+            }) // Set response as state to avoid double state update
+            .catch(_ => {
+                notificationApi.error({
+                    message: "Error",
+                    description: "There was an error sending your message. Please try again later."
+                });
+            })
+            .finally(() => setLoading(false)); // API has responded
+    }
 
     const handleAiFill = () => {
         form.setFieldsValue({ content: "Hello Guillaume!\nThis message has been filled by AI." });
@@ -13,11 +41,13 @@ const ContactPage = () => {
 
     return (
         <Page title="Contact">
+            {notificationHolder}
             <Form form={form}
                 layout="horizontal"
                 style={{ justifySelf: "center", width: "100%", maxWidth: 600 }}>
                 <Form.Item name="email">
                     <Input
+                        disabled
                         placeholder="youremail@email.com"
                         defaultValue={authContext.user?.email} />
                 </Form.Item>
@@ -28,10 +58,16 @@ const ContactPage = () => {
                 </Form.Item>
                 <Form.Item>
                     <Space>
-                        <Button type="primary" htmlType="submit">
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            loading={loading}
+                            onClick={handleSubmit}>
                             Send
                         </Button>
-                        <Button htmlType="button" onClick={handleAiFill}>
+                        <Button
+                            htmlType="button"
+                            onClick={handleAiFill}>
                             AI Fill
                         </Button>
                     </Space>
